0621cd0ccbef512209b368adf008f58b0657cd02
Gregoris <deivypereira@hotmail.com>
2021-09-20 21:52:30 -0500

    Updates

---
+++ precache-manifest.3cccd9b64ce27fff9c181cd709b395bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d041aa1fe341e83d2cb91ba947f294c9",
+    "url": "/DeivyPereira/token-ico-pbv/index.html"
+  },
+  {
+    "revision": "f84b9eeb605ad938fbba",
+    "url": "/DeivyPereira/token-ico-pbv/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "8e9546a5038098b0a45a",
+    "url": "/DeivyPereira/token-ico-pbv/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "f84b9eeb605ad938fbba",
+    "url": "/DeivyPereira/token-ico-pbv/static/js/2.efa1d731.chunk.js"
+  },
+  {
+    "revision": "bd32164b0b1f07ae79545cc1dff28ec5",
+    "url": "/DeivyPereira/token-ico-pbv/static/js/2.efa1d731.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e9546a5038098b0a45a",
+    "url": "/DeivyPereira/token-ico-pbv/static/js/main.a6d319e1.chunk.js"
+  },
+  {
+    "revision": "f37da8bde172fbd06a2f",
+    "url": "/DeivyPereira/token-ico-pbv/static/js/runtime-main.a4690a2d.js"
+  }
+]);
